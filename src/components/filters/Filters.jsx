78bf44791c3b9536ac24ca9f2ba7401405967a07
Johitan <johan52754@gmail.com>
2021-05-18 21:04:59 -0500

    New changes

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -10,15 +10,15 @@ export function Filters(props){
     return (
         <div className="Filter">
             <div className="Filters">
-                <div>
+                <div className="input">
                     <FontAwesomeIcon icon={faSignInAlt}/>
                     <input className="availabilityFrom" onChange={props.stateFrom[1]} value={props.stateFrom[0]}type="date"/>
                 </div>
-                <div>
+                <div className="input">
                     <FontAwesomeIcon icon={faSignOutAlt}/>
                     <input className="availabilityTo" onChange={props.stateTo[1]} value={props.stateTo[0]} type="date"/>
                 </div>
-                <div>
+                <div className="select">
                     <FontAwesomeIcon icon={faGlobe}/>
                     <select onChange={props.stateCountry[1]} value={props.stateCountry[0]} className="country" name="paises" id="">
                         <option value="Cualquier pais">Todos los paises</option>
@@ -28,7 +28,7 @@ export function Filters(props){
                         <option value="Uruguay">Uruguay</option>
                     </select>
                 </div>
-                <div>
+                <div className="select">
                     <FontAwesomeIcon icon={faDollarSign}/>
                     <select onChange={props.statePrice[1]} value={props.statePrice[0]} className="price" name="precio" id="">
                         <option value="Cualquier precio">Cualquier precio</option>
@@ -38,7 +38,7 @@ export function Filters(props){
                         <option value={4}>$$$$</option>
                     </select>
                 </div>
-                <div>
+                <div className="select">
                     <FontAwesomeIcon icon={faBed}/>
                     <select onChange={props.stateSize[1]} value={props.stateSize[0]} className="rooms" name="tamaño" id="">
                         <option value="Cualquier tamaño">Cualquier tamaño</option>
